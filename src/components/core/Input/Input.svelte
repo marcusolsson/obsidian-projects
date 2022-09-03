bff16d1bcc06981f476efe2bd7e3b713ec80c0b2
Marcus Olsson <8396880+marcusolsson@users.noreply.github.com>
2022-09-03 12:31:42 +0200

    Refactor FieldControl
    Fixes #7

---
+++ src/components/core/Input/Input.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let type: string = "text";
+	export let value: string;
+	export let onChange: (value: string) => void;
+
+	function handleInput(event: Event) {
+		if (event.currentTarget instanceof HTMLInputElement) {
+			onChange(event.currentTarget.value);
+		}
+	}
+</script>
+
+<input {value} {type} on:input={handleInput} />
