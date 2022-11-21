815a45f7e61b211cf565ee853d4a74e8aa96bb0e
Marcus Olsson <8396880+marcusolsson@users.noreply.github.com>
2022-11-21 10:25:27 +0100

    Fix priority sorting (#177)

--- src/views/Board/components/Board/BoardColumn.svelte
+++ src/views/Board/components/Board/BoardColumn.svelte
@@ -1,40 +1,27 @@
 <script lang="ts">
   import { Button, InternalLink, Typography } from "obsidian-svelte";
-  import path from "path";
   import { i18n } from "src/lib/stores/i18n";
   import { app } from "src/lib/stores/obsidian";
 
-  import { isNumber, type DataRecord } from "src/lib/data";
+  import type { DataRecord } from "src/lib/data";
   import Card from "../Card/Card.svelte";
   import CardList from "../Card/CardList.svelte";
+  import {
+    getDisplayName,
+    getPrioritizedRecords,
+    getUnprioritizedRecords,
+  } from "src/views/Board/components/Board/board-helpers";
 
   export let name: string;
   export let records: DataRecord[];
   export let groupByPriority: string | undefined;
   export let readonly: boolean;
 
-  $: prioritized = getPrioritizedRecords(records);
-  $: unprioritized = getUnprioritizedRecords(records);
+  $: prioritized = getPrioritizedRecords(records, groupByPriority);
+  $: unprioritized = getUnprioritizedRecords(records, groupByPriority);
 
   export let onRecordClick: (record: DataRecord) => void;
   export let onRecordAdd: () => void;
-
-  function getPrioritizedRecords(records: DataRecord[]): DataRecord[] {
-    return records.filter((record) => {
-      return groupByPriority && isNumber(record.values[groupByPriority]);
-    });
-  }
-
-  function getUnprioritizedRecords(records: DataRecord[]): DataRecord[] {
-    return records.filter((record) => {
-      return groupByPriority && !isNumber(record.values[groupByPriority]);
-    });
-  }
-
-  function getDisplayName(record: DataRecord): string {
-    const basename = path.basename(record.id);
-    return basename.slice(0, basename.lastIndexOf("."));
-  }
 </script>
 
 <div data-id={name} class="column">
---
+++ src/views/Board/components/Board/board-helpers.test.ts
@@ -0,0 +1,50 @@
+import { describe, it, expect } from "@jest/globals";
+
+import type { DataRecord } from "src/lib/data";
+import {
+  getPrioritizedRecords,
+  getUnprioritizedRecords,
+} from "./board-helpers";
+
+describe("board", () => {
+  const records: DataRecord[] = [
+    {
+      id: "D.md",
+      values: { weight: 3, due: new Date(Date.parse("2022-01-03")) },
+    },
+    {
+      id: "B.md",
+      values: { due: new Date(Date.parse("2022-01-01")) },
+    },
+    {
+      id: "A.md",
+      values: { weight: 2, due: new Date(Date.parse("2022-01-02")) },
+    },
+    {
+      id: "C.md",
+      values: { weight: 3 },
+    },
+  ];
+
+  it("should sort prioritized records", () => {
+    expect(getPrioritizedRecords(records, "weight")).toStrictEqual([
+      { ...records[2] },
+      { ...records[3] },
+      { ...records[0] },
+    ]);
+    expect(getPrioritizedRecords(records, "due")).toStrictEqual([
+      { ...records[1] },
+      { ...records[2] },
+      { ...records[0] },
+    ]);
+  });
+
+  it("should sort unprioritized records", () => {
+    expect(getUnprioritizedRecords(records, "weight")).toStrictEqual([
+      { ...records[1] },
+    ]);
+    expect(getUnprioritizedRecords(records, "due")).toStrictEqual([
+      { ...records[3] },
+    ]);
+  });
+});
