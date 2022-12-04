db820bfd17a5a7446c4b48bcf85db83ed4079369
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-12-04 15:07:17 -0500

    add icon to view

--- src/views/Timeline/timeline-view.ts
+++ src/views/Timeline/timeline-view.ts
@@ -17,7 +17,7 @@ export class TimelineView extends ProjectView<TimelineConfig> {
     return "Timeline";
   }
   getIcon(): string {
-    return "align-vertical-distribute-center";
+    return "list-minus";
   }
   async onData({ data }: DataQueryResult): Promise<void> {
     this.view?.$set({ frame: data });
