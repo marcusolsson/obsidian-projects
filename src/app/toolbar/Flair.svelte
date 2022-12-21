124abe51ee2933c2bcfb910cfbfdb2e7a1ebf56e
Marcus Olsson <8396880+marcusolsson@users.noreply.github.com>
2022-12-21 08:55:51 +0100

    Use flair for filter condition count (#252)

--- src/app/toolbar/Flair.svelte
+++ src/app/toolbar/Flair.svelte
@@ -1,8 +1,21 @@
-<span class="flair" on:click on:keypress><slot /></span>
+<script lang="ts">
+  export let variant: "primary" | "error";
+</script>
+
+<span
+  class="flair"
+  class:mod-pop={variant === "primary"}
+  class:error={variant === "error"}
+  on:click
+  on:keypress><slot /></span
+>
 
 <style>
   span {
-    background-color: var(--background-modifier-error);
     color: var(--text-on-accent);
+  }
+
+  .error {
+    background-color: var(--background-modifier-error);
   }
 </style>
