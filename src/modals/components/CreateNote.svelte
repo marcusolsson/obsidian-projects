29e5a09bc0f5fe1c0815dfbbadda934a6ffe0077
Acylation <532117255@qq.com>
2023-02-05 11:08:33 +0800

    Add RegExp for empty chars

--- src/modals/components/CreateNote.svelte
+++ src/modals/components/CreateNote.svelte
@@ -41,7 +41,7 @@
   }
 
   function validateName(name: string) {
-    if (name === "") {
+    if (name === "" || name.match(/^\s*$/)) {
       return $i18n.t("modals.note.create.empty-name-error");
     }
 
