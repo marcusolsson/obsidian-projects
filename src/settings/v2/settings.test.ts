2e549573547aa3d23e9a77a0bdf2966a303fe479
Acylation <532117255@qq.com>
2023-10-05 00:03:35 +0800

    Building tests

--- src/settings/v2/settings.test.ts
+++ src/settings/v2/settings.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "@jest/globals";
 import { resolve } from "./settings";
+import type { ShowCommand } from "../settings";
 
 describe("resolve v2", () => {
   it("resolves minimum", () => {
@@ -52,6 +53,111 @@ describe("resolve v2", () => {
         },
         projectSizeLimit: 1000,
         commands: [],
+        linkBehavior: "open-editor",
+      },
+    });
+  });
+});
+
+describe("clean up commands", () => {
+  it("uniquify commands and remove orphan ones", () => {
+    const validCommands: ShowCommand[] = [
+      { project: "sample-project" },
+      { project: "sample-project", view: "project-view" },
+      { project: "sample-archive" },
+      { project: "sample-archive", view: "archive-view" },
+    ];
+    const invalidCommands: ShowCommand[] = [
+      { project: "unknown-project" },
+      { project: "sample-project", view: "unknown-view" },
+      { project: "unknown-archive" },
+      { project: "sample-archive", view: "unknown-view" },
+    ];
+    const duplicatedCommands: ShowCommand[] = [
+      { project: "sample-project" },
+      { project: "sample-archive", view: "archive-view" },
+    ];
+
+    const got = resolve({
+      version: 2,
+      projects: [
+        {
+          name: "Sample Project",
+          id: "sample-project",
+          fieldConfig: {},
+          defaultName: "",
+          templates: [],
+          excludedNotes: [],
+          isDefault: false,
+          dataSource: {
+            kind: "folder",
+            config: {
+              path: "",
+              recursive: false,
+            },
+          },
+          newNotesFolder: "",
+          views: [
+            {
+              name: "Table",
+              id: "project-view",
+              type: "table",
+              config: {},
+              filter: { conditions: [] },
+              colors: { conditions: [] },
+              sort: { criteria: [] },
+            },
+          ],
+        },
+      ],
+      archives: [
+        {
+          name: "Archived Project",
+          id: "sample-archive",
+          fieldConfig: {},
+          defaultName: "",
+          templates: [],
+          excludedNotes: [],
+          isDefault: false,
+          dataSource: {
+            kind: "folder",
+            config: {
+              path: "",
+              recursive: false,
+            },
+          },
+          newNotesFolder: "",
+          views: [
+            {
+              name: "Table",
+              id: "archive-view",
+              type: "table",
+              config: {},
+              filter: { conditions: [] },
+              colors: { conditions: [] },
+              sort: { criteria: [] },
+            },
+          ],
+        },
+      ],
+      preferences: {
+        projectSizeLimit: 1000,
+        frontmatter: {
+          quoteStrings: "PLAIN",
+        },
+        commands: [...validCommands, ...invalidCommands, ...duplicatedCommands],
+        linkBehavior: "open-editor",
+      },
+    });
+
+    expect(got).toStrictEqual({
+      ...got,
+      preferences: {
+        projectSizeLimit: 1000,
+        frontmatter: {
+          quoteStrings: "PLAIN",
+        },
+        commands: validCommands,
         linkBehavior: "open-editor",
       },
     });
