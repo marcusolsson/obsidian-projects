fc3933900de9358b1be068ad509bc9402af46971
Acylation <532117255@qq.com>
2023-11-14 18:08:08 +0800

    Allow rich-text rendering on board column header

---
+++ src/ui/views/Board/components/Board/ColumnHeader.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { MarkdownRenderer } from "obsidian";
+  import { app, view } from "src/lib/stores/obsidian";
+  import { getContext } from "svelte";
+
+  export let value: string;
+  export let richText: boolean = false;
+
+  const sourcePath = getContext<string>("sourcePath") ?? "";
+
+  function useMarkdown(node: HTMLElement, value: string) {
+    MarkdownRenderer.renderMarkdown(value, node, sourcePath, $view);
+
+    return {
+      update(newValue: string) {
+        node.empty();
+        MarkdownRenderer.renderMarkdown(newValue, node, sourcePath, $view);
+      },
+    };
+  }
+
+  function handleClick(event: MouseEvent) {
+    const targetEl = event.target as HTMLElement;
+    const closestAnchor =
+      targetEl.tagName === "A" ? targetEl : targetEl.closest("a");
+
+    if (!closestAnchor) {
+      return;
+    }
+
+    if (closestAnchor.hasClass("internal-link")) {
+      event.preventDefault();
+
+      const href = closestAnchor.getAttr("href");
+      const newLeaf = event.button === 1 || event.ctrlKey || event.metaKey;
+
+      if (href) {
+        $app.workspace.openLinkText(href, sourcePath, newLeaf);
+      }
+    }
+  }
+</script>
+
+{#if richText}
+  <div use:useMarkdown={value} on:click={handleClick} on:keypress />
+{:else}
+  <div>
+    {value}
+  </div>
+{/if}
+
+<style>
+  div :global(p:first-child) {
+    margin-top: 0;
+  }
+
+  div :global(p:last-child) {
+    margin-bottom: 0;
+  }
+</style>
