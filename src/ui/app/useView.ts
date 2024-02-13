492f0509f6f3feb923ac0d1ced0df065f0264970
st4ng <st4ng@users.noreply.github.com>
2024-02-13 22:35:28 +0100

    Refactor useView to avoid duplicate code

--- src/ui/app/useView.ts
+++ src/ui/app/useView.ts
@@ -1,9 +1,9 @@
 import { get } from "svelte/store";
 
-import type { DataQueryResult } from "src/customViewApi";
+import type { DataQueryResult, ProjectView } from "src/customViewApi";
+import type { DataRecord } from "src/lib/dataframe/dataframe";
 import { customViews } from "src/lib/stores/customViews";
 import type { ViewApi } from "src/lib/viewApi";
-import type { DataRecord } from "src/lib/dataframe/dataframe";
 import type { ProjectDefinition, ViewDefinition } from "src/settings/settings";
 
 export interface ViewProps {
@@ -19,61 +19,48 @@ export interface ViewProps {
 
 export function useView(node: HTMLElement, props: ViewProps) {
   // Keep track of previous view id to determine if view should be invalidated.
-  let viewId = props.view.id;
-
+  let viewId: string;
   const projectId = props.project.id;
+  let projectView: ProjectView<Record<string, any>> | undefined;
 
-  let projectView = get(customViews)[props.view.type];
+  const update = (newprops: ViewProps) => {
+    // User switched to a different view.
+    const dirty =
+      newprops.view.id !== viewId || newprops.project.id !== projectId;
 
-  if (projectView) {
-    // Component just mounted, so treat all properties as dirty.
-    projectView.onOpen({
-      viewId: props.view.id,
-      project: props.project,
-      contentEl: node,
-      config: props.config,
-      saveConfig: props.onConfigChange,
-      viewApi: props.viewApi,
-      readonly: props.readonly,
-      getRecordColor: props.getRecordColor,
-    });
-    projectView.onData(props.dataProps);
-  }
+    if (dirty) {
+      // Clean up previous view.
+      projectView?.onClose();
+
+      node.empty();
+
+      // Look up the next view.
+      projectView = get(customViews)[newprops.view.type];
+
+      if (projectView) {
+        projectView.onOpen({
+          contentEl: node,
+          viewId: newprops.view.id,
+          project: newprops.project,
+          viewApi: newprops.viewApi,
+          readonly: newprops.readonly,
+          config: newprops.config,
+          saveConfig: newprops.onConfigChange,
+          getRecordColor: newprops.getRecordColor,
+        });
+        projectView.onData(newprops.dataProps);
+      }
+
+      viewId = newprops.view.id;
+    } else {
+      projectView?.onData(newprops.dataProps);
+    }
+  };
+
+  update(props);
 
   return {
-    update(newprops: ViewProps) {
-      // User switched to a different view.
-      const dirty =
-        newprops.view.id !== viewId || newprops.project.id !== projectId;
-
-      if (dirty) {
-        // Clean up previous view.
-        projectView?.onClose();
-
-        node.empty();
-
-        // Look up the next view.
-        projectView = get(customViews)[newprops.view.type];
-
-        if (projectView) {
-          projectView.onOpen({
-            contentEl: node,
-            viewId: newprops.view.id,
-            project: newprops.project,
-            viewApi: newprops.viewApi,
-            readonly: newprops.readonly,
-            config: newprops.config,
-            saveConfig: newprops.onConfigChange,
-            getRecordColor: newprops.getRecordColor,
-          });
-          projectView.onData(newprops.dataProps);
-        }
-
-        viewId = newprops.view.id;
-      } else {
-        projectView?.onData(newprops.dataProps);
-      }
-    },
+    update,
     destroy() {
       projectView?.onClose();
     },
