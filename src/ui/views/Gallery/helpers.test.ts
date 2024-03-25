546874d87b6d407489eea636ee4edc10068be9ad
Acylation <532117255@qq.com>
2024-03-25 17:22:25 +0800

    Fix link parsing for gallery view

--- src/ui/views/Gallery/helpers.test.ts
+++ src/ui/views/Gallery/helpers.test.ts
@@ -6,17 +6,63 @@ describe("parseObsidianLink", () => {
     expect(parseObsidianLink("Untitled")).toBeNull();
   });
 
-  it("parses simple link", () => {
+  it("parses simple wiki link", () => {
     expect(parseObsidianLink("[[Untitled]]")).toStrictEqual({
       linkText: "Untitled",
       displayName: "",
     });
   });
 
-  it("parses link with display name", () => {
+  it("parses wiki link with display name", () => {
     expect(parseObsidianLink("[[Untitled|Foo]]")).toStrictEqual({
       linkText: "Untitled",
       displayName: "Foo",
+    });
+  });
+
+  it("parses simple wiki link embed", () => {
+    expect(parseObsidianLink("![[Untitled]]")).toStrictEqual({
+      linkText: "Untitled",
+      displayName: "",
+    });
+  });
+
+  it("parses wiki link embed with display name", () => {
+    expect(parseObsidianLink("![[Untitled|Foo]]")).toStrictEqual({
+      linkText: "Untitled",
+      displayName: "Foo",
+    });
+  });
+
+  it("parse simple markdown link", () => {
+    expect(parseObsidianLink("[](obsidian-icon.png)")).toStrictEqual({
+      linkText: "obsidian-icon.png",
+      displayName: "",
+    });
+  });
+
+  it("parse markdown link with display name", () => {
+    expect(
+      parseObsidianLink("[my awesome cover](obsidian-icon.png)")
+    ).toStrictEqual({
+      linkText: "obsidian-icon.png",
+      displayName: "my awesome cover",
+    });
+  });
+
+  it("parse simple markdown link embed", () => {
+    expect(parseObsidianLink("![](obsidian-icon.png)")).toStrictEqual({
+      linkText: "obsidian-icon.png",
+      displayName: "",
+    });
+  });
+
+  it("parse markdown link embed with display name", () => {
+    expect(
+      parseObsidianLink("[my awesome cover](obsidian-icon.png)")
+    ).toStrictEqual({
+      linkText: "obsidian-icon.png",
+      displayName: "my awesome cover",
     });
   });
 });
