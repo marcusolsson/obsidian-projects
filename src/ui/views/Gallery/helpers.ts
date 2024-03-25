baaf4112862331d06de1e691018633423474be86
Chiyu Liang <532117255@qq.com>
2024-03-25 22:50:25 +0800

    Fix link parsing for Gallery view (#822)

--- src/ui/views/Gallery/helpers.ts
+++ src/ui/views/Gallery/helpers.ts
@@ -1,15 +1,24 @@
-const obsidianLinkRegExp = /^\[\[(.*?)(\|(.*?))?\]\]$/;
+const wikiLinkRegExp = /^\!?\[\[(.*?)(\|(.*?))?\]\]$/;
+const mdLinkRegExp = /^\!?\[([^\[]*)\]\((.*)\)$/;
 
 export function parseObsidianLink(link: string): {
   linkText: string;
   displayName: string;
 } | null {
-  const match = link.match(obsidianLinkRegExp);
+  const wikiLink = link.match(wikiLinkRegExp);
+  const mdLink = link.match(mdLinkRegExp);
 
-  return match
-    ? {
-        linkText: match[1] || "",
-        displayName: match[3] || "",
-      }
-    : null;
+  if (wikiLink) {
+    return {
+      linkText: wikiLink[1] || "",
+      displayName: wikiLink[3] || "",
+    };
+  } else if (mdLink) {
+    return {
+      linkText: mdLink[2] || "",
+      displayName: mdLink[1] || "",
+    };
+  } else {
+    return null;
+  }
 }
