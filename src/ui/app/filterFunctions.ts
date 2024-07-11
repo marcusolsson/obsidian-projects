b0cc65adb3ce8aa48811a7073988a0501ccc8255
Signynt <67801159+Signynt@users.noreply.github.com>
2024-07-11 17:06:17 +0200

    Removed accidentally left in comment

--- src/ui/app/filterFunctions.ts
+++ src/ui/app/filterFunctions.ts
@@ -159,7 +159,6 @@ export const listFns: Record<
 > = {
   "has-any-of": (left, right) => {
     return right ? right.some((value) => left.includes(value)) : false;
-    // return right ? right.some((value) => left.some((l) => l.includes(value))) : false;
   },
   "has-all-of": (left, right) => {
     return right ? right.every((value) => left.includes(value)) : false;
